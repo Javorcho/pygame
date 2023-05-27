ad6ba135758b307d928c978914c0fcd0b03002d3
Javorcho <yavor.minkovski@gmail.com>
2023-05-27 17:05:40 +0300

    Create wordgenerator.py
    This is the code that will be used to both give you a word to search for and to generate the words that you will be looking around

---
+++ pygame/wordgenerator.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_random_word():
+    word_list = ["apple", "banana", "carrot", "dog", "elephant", "fish", "giraffe", "horse", "iguana", "jellyfish","nigga","ye","biden","malta","steak","fried eggs","loaf","tues","crystal meth"]
+    random_word = random.choice(word_list)
+    return random_word
+
+
+random_word = generate_random_word()
+print("Random Word:", random_word)
