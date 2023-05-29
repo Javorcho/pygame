df6eade0949b425bd5a8ed402cbb1df11bd441dd
Javorcho <yavor.minkovski@gmail.com>
2023-05-29 10:36:11 +0300

    Update pygame.py
    added the second menu: the one that shows you what word you must look for

--- pygame/pygame.py
+++ pygame/pygame.py
@@ -2,28 +2,50 @@ import tkinter as tk
 from tkinter import messagebox
 
 def start_game():
-    messagebox.showinfo("Find Wordo", "Starting the game...")
-  
+    fade_window = tk.Toplevel(window)
+    fade_window.title("Fading Screen")
+    fade_window.geometry("900x600")
+    fade_window.configure(bg="black")
+    fade_label = tk.Label(fade_window, text="Starting the game...", font=("Terminus", 32), bg="black", fg="yellow")
+    fade_label.pack(expand=True)
+    fade_window.attributes('-alpha', 0.0)
+
+    def fade_in():
+        alpha = fade_window.attributes('-alpha')
+        if alpha < 1.0:
+            alpha += 0.1
+            fade_window.attributes('-alpha', alpha)
+            fade_window.after(100, fade_in)
+        else:
+            fade_label.configure(text="Your word is: Wordo")
+            fade_window.after(2000, fade_out)
+
+    def fade_out():
+        alpha = fade_window.attributes('-alpha')
+        if alpha > 0.0:
+            alpha -= 0.1
+            fade_window.attributes('-alpha', alpha)
+            fade_window.after(100, fade_out)
+        else:
+            fade_window.destroy()
+
+    fade_in()
 
 def quit_program():
     window.destroy()
-
 
 window = tk.Tk()
 window.title("Find Wordo")
 window.geometry("900x600")
 window.configure(bg="black")
 
+title_label = tk.Label(window, text="FIND WORDO", font=("Terminus", 32), bg="black", fg="yellow")
+title_label.pack(pady=80)
 
-title_label = tk.Label(window, text="FIND WORDO", font=("Terminus", 16), bg="black", fg="yellow")
-title_label.pack(pady=40)
-
-
-start_button = tk.Button(window, text="Start Game", font=("Terminus", 14), bg="black", fg="yellow", command=start_game)
+start_button = tk.Button(window, text="Start Game", font=("Terminus", 28), bg="black", fg="yellow", command=start_game)
 start_button.pack(pady=20)
 
-quit_button = tk.Button(window, text="Quit", font=("Terminus", 14), bg="black", fg="yellow", command=quit_program)
+quit_button = tk.Button(window, text="Quit", font=("Terminus", 28), bg="black", fg="yellow", command=quit_program)
 quit_button.pack(pady=20)
-
 
 window.mainloop()
