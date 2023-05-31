23fe6b981bfafc0049d82aeac90f361b3816b566
Javorcho <yavor.minkovski@gmail.com>
2023-05-31 11:24:08 +0300

    pre final
    fixed the test game, which is almost ready

--- pygame/mygame.py
+++ pygame/mygame.py
@@ -28,38 +28,7 @@ STATE_GAME = 1
 # Current game state
 current_state = STATE_MENU
 
-# Current difficulty level
-current_difficulty = ""
-
-# Difficulty levels
-difficulty_levels = {
-    "easy": {
-        "name": "Easy",
-        "word_count": 10,
-        "fade_in_duration": 2000,
-        "fade_out_duration": 2000
-    },
-    "medium": {
-        "name": "Medium",
-        "word_count": 20,
-        "fade_in_duration": 1500,
-        "fade_out_duration": 1500
-    },
-    "hard": {
-        "name": "Hard",
-        "word_count": 30,
-        "fade_in_duration": 1000,
-        "fade_out_duration": 1000
-    },
-    "extreme": {
-        "name": "Extreme",
-        "word_count": 40,
-        "fade_in_duration": 500,
-        "fade_out_duration": 500
-    }
-}
-
-def fade_in(difficulty):
+def fade_in():
     fade_label.set_text("Starting the game...")
     fade_label.update()
 
@@ -72,9 +41,9 @@ def fade_in(difficulty):
     fade_label.set_text(f"Your word is: {word}")
     fade_label.update()
     pygame.time.wait(2000)
-    fade_out(difficulty)
+    fade_out()
 
-def fade_out(difficulty):
+def fade_out():
     for i in range(10, -1, -1):
         fade_label.set_color((i * 2, i * 2, 0))
         fade_label.update()
@@ -83,46 +52,13 @@ def fade_out(difficulty):
     fade_label.set_text("")
     fade_label.update()
 
-    # Display the difficulty buttons
-    difficulty_buttons = []
-    button_x = 325
-    button_y = 180
-    button_width = 100
-    button_height = 50
-    button_margin = 20
-
-    for level in difficulty_levels:
-        difficulty_button = pygame.Rect(button_x, button_y, button_width, button_height)
-        difficulty_buttons.append(difficulty_button)
-
-        button_text = font.render(difficulty_levels[level]["name"], True, BLACK)
-        button_text_rect = button_text.get_rect(center=(difficulty_button.x + button_width // 2, difficulty_button.y + button_height // 2))
-        window.blit(button_text, button_text_rect)
-
-        button_x += button_width + button_margin
-
-    pygame.display.flip()
-
-    # Wait for difficulty button click
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                for index, button in enumerate(difficulty_buttons):
-                    if button.collidepoint(event.pos):
-                        current_difficulty = list(difficulty_levels.keys())[index]
-                        print("Selected Difficulty:", current_difficulty)
-                        return
-
 def show_instructions():
     instructions = "Welcome to Find Wordo!\n\n"
     instructions += "The objective of the game is to find the word you were given.\n"
     instructions += "The time you need to find the word will be recorded and displayed in the highscore menus.\n"
-    instructions += "First, click the Start button.\n"
+    instructions += "First click the Start button.\n"
     instructions += "After that, a random word will be generated.\n"
-    instructions += "Then, your screen will fill with words, and you will have to find the one you were given.\n"
+    instructions += "Then, your screen will fill with words and you will have to find the one you were given.\n"
     instructions += "Have fun and enjoy playing Find Wordo!\n"
     instructions += "If you want to add or remove words, you must go to the wordgenerator file and make changes."
 
@@ -164,28 +100,29 @@ while running:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if current_state == STATE_MENU:
-                if start_button.collidepoint(event.pos):
+                mouse_pos = pygame.mouse.get_pos()
+                if start_button.collidepoint(mouse_pos):
                     current_state = STATE_GAME
-                    fade_in(current_difficulty)
-                elif instructions_button.collidepoint(event.pos):
+                    fade_in()
+                elif instructions_button.collidepoint(mouse_pos):
                     show_instructions()
-                elif quit_button.collidepoint(event.pos):
+                elif quit_button.collidepoint(mouse_pos):
                     running = False
 
     # Clear the window
     window.fill(BLACK)
 
     if current_state == STATE_MENU:
-        # Draw the menu screen
-        window.blit(title_label, (350, 150))
+        # Draw the menu elements
+        window.blit(title_label, (350, 200))
         pygame.draw.rect(window, YELLOW, start_button)
-        window.blit(start_text, (start_button.x + 50, start_button.y + 10))
+        window.blit(start_text, (start_button.x + 30, start_button.y + 10))
         pygame.draw.rect(window, YELLOW, instructions_button)
-        window.blit(instructions_text, (instructions_button.x + 25, instructions_button.y + 10))
+        window.blit(instructions_text, (instructions_button.x + 30, instructions_button.y + 10))
         pygame.draw.rect(window, YELLOW, quit_button)
         window.blit(quit_text, (quit_button.x + 60, quit_button.y + 10))
     elif current_state == STATE_GAME:
-        # Draw the game screen
+        # Draw the game elements
         sprites.draw(window)
 
     # Update the display
--- pygame/test.py
+++ pygame/test.py
@@ -2,6 +2,14 @@ import tkinter as tk
 from tkinter import messagebox
 from wordgenerator import generate_random_word
 
+DIFFICULTIES = {
+    "Easy": 20,
+    "Medium": 50,
+    "Hard": 100
+}
+
+target_word = ""
+
 def fade_in():
     fade_label.configure(text="Starting the game...")
     fade_label.update()
@@ -11,103 +19,109 @@ def fade_in():
         fade_label.update()
         fade_label.after(200)
 
+    word = generate_random_word()
+    fade_label.configure(text=f"Your word is: {word}")
+    fade_label.update()
+    fade_label.after(2000, fade_out)
+
+def fade_out():
+    for i in range(10, -1, -1):
+        fade_label.configure(bg=f"#{i*2:02x}{i*2:02x}00", fg="black" if i <= 5 else "yellow")
+        fade_label.update()
+        fade_label.after(200)
+
     fade_label.configure(text="")
     fade_label.update()
-
-    show_difficulty_menu()  # Show the difficulty menu after the word menu disappears
+    show_difficulty_menu()
 
 def show_instructions():
     instructions = "Welcome to Find Wordo!\n\n"
     instructions += "The objective of the game is to find the word you were given.\n"
-    instructions += "The time you need to find the word will be recorded and displayed in the highscore menus.\n"
-    instructions += "First, click the start button.\n"
+    instructions += "First click the start button.\n"
     instructions += "After that, a random word will be generated.\n"
-    instructions += "Then, your screen will fill with words and you will have to find the one you were given.\n"
+    instructions += "Then your screen will fill with words and you will have to find the one you were given.\n"
+    instructions += "There are 3 levels of difficulty:"
+    instructions += "Easy = 20 words"
+    instructions += "Medium = 50 words"
+    instructions += "Hard = 100 words"
     instructions += "Have fun and enjoy playing Find Wordo!\n"
-    instructions += "If you want to add or remove words, you must go to the wordgenerator file where you can modify the word list."
+    instructions += "If you want to add or remove words, you must go to the wordgenerator file and modify the word list."
 
     messagebox.showinfo("Instructions", instructions)
 
 def show_difficulty_menu():
-    # Hide the start button and instructions button
+    
     start_button.pack_forget()
     instructions_button.pack_forget()
 
-    # Create a new label for the difficulty menu
+   
     difficulty_label = tk.Label(window, text="Select Difficulty:", font=("Terminus", 28), bg="black", fg="yellow")
-    difficulty_label.pack(pady=80)
+    difficulty_label.pack(pady=60)
 
-    # Create buttons for each difficulty level
-    easy_button = tk.Button(window, text="Easy", font=("Terminus", 24), bg="black", fg="yellow", command=start_game_easy)
-    easy_button.pack(pady=20)
+    
+    for difficulty, num_words in DIFFICULTIES.items():
+        button = tk.Button(window, text=difficulty, font=("Terminus", 24), bg="black", fg="yellow",
+                           command=lambda n=num_words: start_game(n))
+        button.pack(pady=10)
 
-    medium_button = tk.Button(window, text="Medium", font=("Terminus", 24), bg="black", fg="yellow", command=start_game_medium)
-    medium_button.pack(pady=20)
+def start_game(num_words):
+    difficulty_label.pack_forget() 
+    title_label.pack_forget()
+    for button in difficulty_buttons:  
+        button.pack_forget()
 
-    hard_button = tk.Button(window, text="Hard", font=("Terminus", 24), bg="black", fg="yellow", command=start_game_hard)
-    hard_button.pack(pady=20)
+    word_list = generate_word_list(num_words)
+    target_word = generate_random_word()
+    fade_label.configure(text=f"Find: {target_word}")
+    fade_label.update()
+    fade_label.after(2000, lambda: fill_screen_with_words(word_list))
+    fade_label.configure(bg="black", fg="black")  
 
-    extreme_button = tk.Button(window, text="Extreme", font=("Terminus", 24), bg="black", fg="yellow", command=start_game_extreme)
-    extreme_button.pack(pady=20)
+def show_difficulty_menu():
+    start_button.pack_forget()
+    instructions_button.pack_forget()
 
-    monster_button = tk.Button(window, text="Monster", font=("Terminus", 24), bg="black", fg="yellow", command=start_game_monster)
-    monster_button.pack(pady=20)
+    global difficulty_label, difficulty_buttons
+    difficulty_label = tk.Label(window, text="Select Difficulty:", font=("Terminus", 28), bg="black", fg="yellow")
+    difficulty_label.pack(pady=60)
+
+    difficulty_buttons = []
+    for difficulty, num_words in DIFFICULTIES.items():
+        button = tk.Button(window, text=difficulty, font=("Terminus", 24), bg="black", fg="yellow",
+                           command=lambda n=num_words: start_game(n))
+        button.pack(pady=10)
+        difficulty_buttons.append(button)
 
 def generate_word_list(num_words):
     word_list = [generate_random_word() for _ in range(num_words)]
     return word_list
 
 def fill_screen_with_words(word_list):
-    # Set the background color of the window to black
     window.configure(bg="black")
 
-    # Clear the previous labels
     for label in word_labels:
         label.destroy()
     word_labels.clear()
 
-    # Create labels for each word and place them on the screen
     for i, word in enumerate(word_list):
         label = tk.Label(window, text=word, font=("Terminus", 16), bg="black", fg="white")
         label.place(relx=(i % 10) * 0.1, rely=(i // 10) * 0.1)
-        label.bind("<Button-1>", word_clicked)  # Bind the click event to the label
+        label.bind("<Button-1>", word_clicked)
         word_labels.append(label)
 
 def word_clicked(event):
     clicked_word = event.widget.cget("text")
-    print("Clicked word:", clicked_word)  # Replace with your logic for handling the clicked word
-
-def start_game_easy():
-    num_words = 20
-    word_list = generate_word_list(num_words)
-    fill_screen_with_words(word_list)
-
-def start_game_medium():
-    num_words = 50
-    word_list = generate_word_list(num_words)
-    fill_screen_with_words(word_list)
-
-def start_game_hard():
-    num_words = 100
-    word_list = generate_word_list(num_words)
-    fill_screen_with_words(word_list)
-
-def start_game_extreme():
-    num_words = 200
-    word_list = generate_word_list(num_words)
-    fill_screen_with_words(word_list)
-
-def start_game_monster():
-    num_words = 1000
-    word_list = generate_word_list(num_words)
-    fill_screen_with_words(word_list)
+    if clicked_word == target_word:
+        messagebox.showinfo("Congratulations!", "You found the word!")
+    else:
+        print("Clicked word:", clicked_word)
 
 def quit_program():
     window.destroy()
 
 window = tk.Tk()
 window.title("Find Wordo")
-window.geometry("900x600")  # Set the initial size of the window
+window.geometry("900x600")
 window.configure(bg="black")
 
 title_label = tk.Label(window, text="FIND WORDO", font=("Terminus", 32), bg="black", fg="yellow")
@@ -122,9 +136,10 @@ instructions_button.pack(pady=20)
 quit_button = tk.Button(window, text="Quit", font=("Terminus", 28), bg="black", fg="yellow", command=quit_program)
 quit_button.pack(pady=20)
 
+
 fade_label = tk.Label(window, text="", font=("Terminus", 32), bg="black", fg="yellow")
 fade_label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-word_labels = []  # Store the labels for the words on the screen
+word_labels = []  
 
 window.mainloop()
