21989edd499006c40fbac517a8faeeab6c416263
Javorcho <yavor.minkovski@gmail.com>
2023-05-31 21:26:58 +0300

    nothing
    just added a word to the word generator

--- pygame/wordgenerator.py
+++ pygame/wordgenerator.py
@@ -1,7 +1,7 @@
 import random
 
 def generate_random_word():
-    word_list = ["apple", "banana", "carrot", "dog", "elephant", "fish", "giraffe", "horse", "iguana", "jellyfish","nice","ye","biden","malta","steak","fried eggs","loaf","tues","crystal meth"]
+    word_list = ["apple", "banana", "carrot", "dog", "elephant", "fish", "giraffe", "horse", "iguana", "jellyfish","nice","ye","biden","malta","steak","fried eggs","loaf","tues","crystal meth","watermelon"]
     random_word = random.choice(word_list)
     return random_word
 
